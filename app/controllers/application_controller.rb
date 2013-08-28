d00a0605ca394efbaec087d30a6dcf13f3485f02
Bruce McElroy <bruce.mcelroy@gmail.com>
2013-08-27 23:55:48 -0400

    removed ckeditor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  respond_to :html, :json
-  
-  # before_filter :find_asset, :only => [:destroy]
-  before_filter :ckeditor_authorize!
 
-  protected
-    
-    def respond_with_asset(asset)
-      file = params[:CKEditor].blank? ? params[:qqfile] : params[:upload]
-      asset.data = Ckeditor::Http.normalize_param(file, request)
-
-      callback = ckeditor_before_create_asset(asset)
-
-      if callback && asset.save
-        body = params[:CKEditor].blank? ? asset.to_json(:only=>[:id, :type]) : %Q"<script type='text/javascript'>
-          window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{Ckeditor::Utils.escape_single_quotes(asset.url_content)}');
-        </script>"
-        
-        render :text => body
-      else
-        render :nothing => true
-      end
-    end
 end
