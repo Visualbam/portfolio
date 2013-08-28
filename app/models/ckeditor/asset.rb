9b6da1c6989b65afe37174d361cd07d5762bd32f
Bruce McElroy <bruce.mcelroy@gmail.com>
2013-08-28 00:34:15 -0400

    fix

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,5 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::Backend::ActiveRecord
-
   delegate :url, :current_path, :content_type, :to => :data
   
   validates_presence_of :data
