a1b73dc58f2b34492f17dab51414dbb20a8b42da
Bruce McElroy <bruce.mcelroy@Gmail.com>
2013-09-04 15:17:02 -0400

    fixed styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
   has_many :comments, :dependent => :destroy, :order => "created_at DESC"
 
   # In plain terms there must be a title and body and the title must be unique.
-  validates :title, :image, :presence => {:message => "field can not be blank"}
+  validates :title, :presence => {:message => "field can not be blank"}
   validates_uniqueness_of :title
 
   mount_uploader :image, ImageUploader
