40972d12a6198d69cbde54e2d48a7cf368757efd
Bruce McElroy <bruce.mcelroy@Gmail.com>
2013-10-18 10:40:13 -0400

    turbolinks first?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require ckeditor-jquery
 //= require ckeditor/basepath
 //= require ckeditor/config
 //= require articles
-//= require turbolinks
 //= require stickyFooter
 //= require scripts.js
 //= require scrollTo.js
