78d20cfa4b3b408db4d39a2eba9439d68651cee7
Bruce McElroy <bruce.mcelroy@gmail.com>
2014-08-24 17:41:33 -0400

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Portfolio::Application.routes.draw do
   
   
-  root :to => "static_pages#home"
+  # root :to => "static_pages#home"
   # Static pages
   # get "/resume" => 'static_pages#resume'
   # get "/portfolio" => 'static_pages#portfolio'
