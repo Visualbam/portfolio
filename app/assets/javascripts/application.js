ea5ce0206ccf3945c0f5042fa95e3568435c8a24
Bruce McElroy <bruce.mcelroy@gmail.com>
2014-08-24 17:46:50 -0400

    removed turbo links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,17 +7,19 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
 //
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
-//= require ckeditor-jquery
-//= require ckeditor/basepath
-//= require ckeditor/config
-//= require articles
-//= require stickyFooter
-//= require scripts.js
-//= require scrollTo.js
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./app
 
+// for more details see: http://emberjs.com/guides/application/
+App = Ember.Application.create({rootElement: '#ember-app'});
+
+//= require_tree .
