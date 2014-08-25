6f0ff68b518a8336b2ad1440c8b23f052a7ae110
Bruce McElroy <bruce.mcelroy@gmail.com>
2014-08-25 16:27:23 -0400

    updated guy

--- app/assets/javascripts/routes/company.js
+++ app/assets/javascripts/routes/company.js
@@ -2,5 +2,7 @@
 // The this.store.find function gets a record by its id. It actually returns a promise which Ember will attempt to resolve.
 
 App.CompanyRoute = Ember.Route.extend({
-  model: function(params) { return this.store.find('company', params.id) }
+  model: function(params) {
+    return this.store.find('company', params.id) 
+  }
 });
